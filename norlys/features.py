535cf0c310721c68e42e242d6c4b03d69170929e
Hugo Lageneste <hugo.lageneste@pm.me>
2023-10-02 18:46:15 +0200

    Fix issue with rolling window components

--- norlys/features.py
+++ norlys/features.py
@@ -5,6 +5,7 @@ from norlys.data_utils import get_formatted_data
 # - isolation forest number of anomalies + percentile of number of anomalies in event
 # - "patterns" on the magnetometre circular map for each class, e.g. arc for "build-up", covers the whole sky for "explosion"
 # - general "disturbance" score based on the deflection score and the anomaly score + the class
+# - mean of the value over 45 mn
 #
 # Features to be added to the model
 # - length of build-up if one occured in the past 45 minutes
