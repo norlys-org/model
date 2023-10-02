bfefad7e5473c85384cb62fe4d78758d539811ff
Hugo Lageneste <hugo.lageneste@pm.me>
2023-10-02 12:48:28 +0200

    Create model training function

---
+++ norlys /model.py
@@ -0,0 +1,21 @@
+import data_utils as dutils
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.metrics import accuracy_score
+import logging
+
+def train_0m_classifier(log=True):
+	"""
+	Train the '0m' classifier, i.e. the model predicting the label for the present. 
+	"""
+
+	logging.info('Gathering training data...')
+	X_train, X_test, y_train, y_test = dutils.get_training_data('label')
+
+	logging.info('Training classifier...')
+	clf = ExtraTreesClassifier(n_estimators=100, random_state=42)
+	clf.fit(X_train, y_train)
+	
+	logging.info('Running tests to compute accuracy score...')
+	y_pred = clf.predict(X_test)
+	accuracy = accuracy_score(y_test, y_pred)
+	logging.info(f'Trained ExtraTreesClassifier 0m, accurary: {accuracy * 100}%')
