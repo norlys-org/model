8365fce603fa337a9e07b4f17d1ba092c76b2708
Hugo Lageneste <hugo.lageneste@pm.me>
2024-05-15 15:52:27 +0200

    Retrain the model

--- norlys/features/features.py
+++ norlys/features/features.py
@@ -1,18 +1,16 @@
 from sklearn.ensemble import IsolationForest
 
-def anomaly(df, component):
-  isolation_forest = IsolationForest(random_state=42)
-  X = df[[component]].values 
-  isolation_forest.fit(X)
-
-  return isolation_forest.predict(X)
-
 def rolling_anomalies(df, component):
   """
   Number of anomalies over the past 15 minutes
   Depends on IsolationForest anomalies to be computed
   """
 
+  isolation_forest = IsolationForest(random_state=42)
+  X = df[[component]].values 
+  isolation_forest.fit(X)
+
+  df[f'{component}_anomaly'] = isolation_forest.predict(X)
   return (df[f'{component}_anomaly'] == -1).astype(int).rolling('15min').sum()
 
 def rolling_gradient(df, component):
@@ -34,7 +32,6 @@ def mean(df, component):
 
 # the order matters due to dependencies
 features = {
-  'anomaly': anomaly,
   'rolling_anomalies': rolling_anomalies,
   'rolling_gradient': rolling_gradient,
   'deflection': deflection,
