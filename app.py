b8cc4585cf782ad63ea32a482d77212fb742caa4
Hugo Lageneste <hugo.lageneste@pm.me>
2024-09-06 01:45:59 +0200

    Add lo-res and hi-res model

--- app.py
+++ app.py
@@ -6,14 +6,7 @@ from pysecs import SECS
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-  body = request.json
-  x = np.array(body['x'])
-  y = np.array(body['y'])
-  i = np.array(body['i'])
-  j = np.array(body['j'])
-
+def interpolate(x, y, i, j, res_lat = 25, res_lon = 50):
   R_earth = 6371e3
   # SECS grid setup within the range of input data
   lat, lon, r = np.meshgrid(np.linspace(50, 90),
@@ -38,8 +31,8 @@ def predict():
   # lat_pred, lon_pred, r_pred = np.meshgrid(np.linspace(50, 85, 100),
   #                                         np.linspace(-80, 40, 200),
   #                                         R_earth, indexing='ij')
-  lat_pred, lon_pred, r_pred = np.meshgrid(np.linspace(50, 85, 25),
-                                          np.linspace(-80, 40, 50),
+  lat_pred, lon_pred, r_pred = np.meshgrid(np.linspace(50, 85, res_lat),
+                                          np.linspace(-80, 40, res_lon),
                                           R_earth, indexing='ij')
   pred_lat_lon_r = np.hstack((lat_pred.reshape(-1, 1),
                               lon_pred.reshape(-1, 1),
@@ -58,13 +51,34 @@ def predict():
   flat_lat = lat_pred.flatten()
   flat_i = i_pred.flatten()
   flat_j = j_pred.flatten()
+  
+  return (flat_lon, flat_lat, flat_i, flat_j)
 
-  return [{
-    'lon': round(flat_lon[i], 2), 
-    'lat': round(flat_lat[i], 2), 
-    'i': round(flat_i[i]), 
-    'j': round(flat_j[i])
-    } for i in range(len(i_pred.flatten()))]
+@app.route('/predict', methods=['POST'])
+def predict():
+  body = request.json
+  x = np.array(body['x'])
+  y = np.array(body['y'])
+  i = np.array(body['i'])
+  j = np.array(body['j'])
+
+  (flat_lon, flat_lat, flat_i, flat_j) = interpolate(x, y, i, j)
+  (flat_lon_h, flat_lat_h, flat_i_h, flat_j_h) = interpolate(x, y, i, j, 100, 200)
+
+  return {
+    "lo-res": [{
+      'lon': round(flat_lon[i], 2), 
+      'lat': round(flat_lat[i], 2), 
+      'i': round(flat_i[i]), 
+      'j': round(flat_j[i])
+    } for i in range(len(flat_i))],
+    "hi-res": [{
+      'lon': round(flat_lon_h[i], 2), 
+      'lat': round(flat_lat_h[i], 2), 
+      'i': round(flat_i_h[i]), 
+      'j': round(flat_j_h[i])
+    } for i in range(len(flat_i))]
+  }
 
 if __name__ == "__main__":
   serve(app, host='0.0.0.0', port=8080)
