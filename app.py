6747ba9167d428bf773af508533a4c467675502c
Hugo Lageneste <hugo.lageneste@pm.me>
2024-10-08 13:08:13 +0200

    Revert "Increase resolution"
    This reverts commit 05347adcb735f5be329a3ae399c6232b0f4c04aa.

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ def predict():
       np.array(body['y'], dtype=np.float32), 
       np.array(body['i'], dtype=np.float32), 
       np.array(body['j'], dtype=np.float32), 
-      50, 
-      100
+      37, 
+      75
   )
 
   # Round the output arrays using numpy's vectorized operations
