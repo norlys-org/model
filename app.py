cbf3a787d71792817e0205d20ed4cf2d1f64a289
Hugo Lageneste <hugo.lageneste@pm.me>
2025-01-20 14:31:29 +0100

    Try revert

--- app.py
+++ app.py
@@ -48,7 +48,9 @@ def interpolate(x, y, i, j, res_lat = 25, res_lon = 50):
 
   # Prediction grid setup
   lat_pred = np.linspace(45, 85, res_lat)  # Create latitude array
-  lon_pred = np.linspace(-180, 180, res_lon)  # Create longitude array
+  lon_pred = np.linspace(-170, 50, res_lon)  # Create longitude array
+  # lat_pred = np.linspace(45, 85, res_lat)  # Create latitude array
+  # lon_pred = np.linspace(-180, 180, res_lon)  # Create longitude array
   # lon_pred = np.linspace(-170, 50, res_lon)  # Create longitude array
   pred_lat_lon_r = np.array([[lt, ln, R_earth] for lt in lat_pred for ln in lon_pred])  # Combine lat, lon, and r into one array
 
@@ -76,7 +78,8 @@ def predict():
       np.array(body['i'], dtype=np.float32), 
       np.array(body['j'], dtype=np.float32), 
       37, 
-      150
+      75
+      # 150
   )
 
   flat_lon2, flat_lat2, flat_d, flat_j2 = interpolate(
@@ -85,7 +88,8 @@ def predict():
       np.array(body['d'], dtype=np.float32), 
       np.array(body['d'], dtype=np.float32), 
       37, 
-      150
+      75
+      # 150
   )
 
   # Round the output arrays using numpy's vectorized operations
