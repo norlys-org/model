2661d26de5c5f3c0bdc5e70e4c3c4954aac28dc6
Hugo Lageneste <hugo.lageneste@pm.me>
2025-01-22 10:17:23 +0100

    Increase resolution

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def interpolate(x, y, i, j, res_lat = 25, res_lon = 50):
   # lon_pred = np.linspace(-170, 50, res_lon)  # Create longitude array
   lat_pred = np.linspace(45, 85, res_lat)  # Create latitude array
   # lon_pred = np.linspace(-180, 50, res_lon)  # Create longitude array
-  lon_pred = np.linspace(-180, 180, res_lon)  # Create longitude array
+  lon_pred = np.linspace(-180, 179, res_lon)  # Create longitude array
   pred_lat_lon_r = np.array([[lt, ln, R_earth] for lt in lat_pred for ln in lon_pred])  # Combine lat, lon, and r into one array
 
   # Predict using the SECS model
@@ -78,8 +78,7 @@ def predict():
       np.array(body['i'], dtype=np.float32), 
       np.array(body['j'], dtype=np.float32), 
       37, 
-      130
-      # 150
+      150
   )
 
   flat_lon2, flat_lat2, flat_d, flat_j2 = interpolate(
@@ -88,8 +87,7 @@ def predict():
       np.array(body['d'], dtype=np.float32), 
       np.array(body['d'], dtype=np.float32), 
       37, 
-      130
-      # 150
+      150
   )
 
   # Round the output arrays using numpy's vectorized operations
