51342aeeebbc7c5e9eb144190f59863bb8cddf6f
Hugo Lageneste <hugo@testnor.com>
2025-04-16 17:44:18 +0200

    Create matrix multiplication

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod grid;
 mod matrix;
 mod secs;
 mod sphere;
+mod svd;
 
 fn main() {}
--- src/svd.rs
+++ src/svd.rs
@@ -1,3 +1,8 @@
-fn solve_svd(a: Vec<Vec<f32>>, b: Vec<Vec<f32>>, epsilon: f32) -> Vec<f32> {
+// use crate::matrix::{transpose_matrix, multiply_matrices};
 
-}
+// fn solve_svd(a: Vec<Vec<f32>>, b: Vec<Vec<f32>>, epsilon: f32) -> Vec<f32> {
+    // let at = transpose_matrix(a);
+    // let ata = multiply_matrices(at, a);
+    //
+    // let max_diag = ata.iter().enumerate().fold(0./0., f32::max);
+// }
