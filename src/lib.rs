4f36573a71aa7b96b169da2aa1a48a36f6535f6e
Hugo Lageneste <hugo@testnor.com>
2025-04-25 14:16:07 +0200

    Revert protobufs usage

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,17 @@ pub mod secs;
 mod sphere;
 mod svd;
 
-use prost::Message;
-use protobufs::{ObservationMatrix, PredictionMatrix};
-use secs::secs_interpolate;
+use secs::{secs_interpolate, ObservationMatrix, PredictionVector};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn infer(bytes: &[u8]) -> Result<JsValue, JsValue> {
+pub fn infer(js_obs: JsValue) -> Result<JsValue, JsValue> {
     console_error_panic_hook::set_once();
+    let observations: ObservationMatrix =
+        serde_wasm_bindgen::from_value(js_obs).expect("Failed to deserialize observations");
 
-    web_sys::console::log_1(&format!("{:?}", bytes).into());
-    let observations = ObservationMatrix::decode(bytes)
-        .map_err(|e| JsValue::from_str(&format!("Failed to deserialize observations: {}", e)))?;
-
-    web_sys::console::log_1(&format!("{:?}", observations).into());
     let pred = secs_interpolate(
-        observations.matrix,
+        observations,
         45f32..85f32,
         37,
         -180f32..179f32,
@@ -28,9 +23,5 @@ pub fn infer(bytes: &[u8]) -> Result<JsValue, JsValue> {
         0f32,
     );
 
-    let mut buf: Vec<u8> = vec![];
-    let matrix = PredictionMatrix { matrix: pred };
-    matrix.encode(&mut buf).unwrap();
-
-    Ok(serde_wasm_bindgen::to_value(&buf)?)
+    Ok(serde_wasm_bindgen::to_value(&pred)?)
 }
