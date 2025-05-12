88dd638ca474863bc1519f0100745f472974ce48
Hugo Lageneste <hugo@testnor.com>
2025-05-12 14:30:02 +0200

    Link secs algorithm in lib infer

--- src/lib.rs
+++ src/lib.rs
@@ -6,37 +6,35 @@ pub mod secs;
 mod sphere;
 mod svd;
 
-use secs::{ObservationMatrix, ObservationVector};
+use grid::geographical_grid;
+use secs::{ObservationMatrix, ObservationVector, SECS};
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
-// #[wasm_bindgen]
-// pub fn infer(js_obs: JsValue) -> Result<JsValue, JsValue> {
-//     console_error_panic_hook::set_once();
-//     let observations: ObservationMatrix =
-//         serde_wasm_bindgen::from_value(js_obs).expect("Failed to deserialize observations");
-//
-//     let pred = secs_interpolate(
-//         observations,
-//         45f32..85f32,
-//         37,
-//         -180f32..179f32,
-//         130,
-//         110e3,
-//         0f32,
-//     );
-//
-//     let pred_score: Vec<f32> = pred
-//         .into_iter()
-//         .map(|v| {
-//             // encode_score(
-//             // apply_auroral_zone_overlay(v.lon, v.lat, ponderate_i(v.i))
-//             v.i
-//             //     false, // TODO: Leave to false as of now derivative is not computed
-//             // )
-//         })
-//         .collect();
-//
-//     Ok(serde_wasm_bindgen::to_value(&pred_score)?)
-// }
+#[wasm_bindgen]
+pub fn infer(js_obs: JsValue) -> Result<JsValue, JsValue> {
+    console_error_panic_hook::set_once();
+    let observations: ObservationMatrix =
+        serde_wasm_bindgen::from_value(js_obs).expect("Failed to deserialize observations");
+
+    let grid = geographical_grid(45.0..85.0, 37, -180.0..179.0, 130, 110.0);
+
+    let mut secs = SECS::new(grid.clone());
+    secs.fit(&observations, 0.1);
+    let pred = secs.predict(&grid).unwrap();
+
+    let pred_score: Vec<f32> = pred
+        .into_iter()
+        .map(|v| {
+            // encode_score(
+            // apply_auroral_zone_overlay(v.lon, v.lat, ponderate_i(v.i))
+            v.i
+            //     false, // TODO: Leave to false as of now derivative is not computed
+            // )
+        })
+        .collect();
+
+    Ok(serde_wasm_bindgen::to_value(&pred_score)?)
+}
 
 #[cfg(test)]
 mod tests {
@@ -58,6 +56,13 @@ mod tests {
 
         let mut secs = SECS::new(grid.clone());
         secs.fit(&obs, 0.1);
-        secs.predict_b(&grid);
+        match secs.predict(&grid) {
+            Ok(r) => {
+                println!("{:?}", r);
+            }
+            Err(_) => {
+                //t
+            }
+        }
     }
 }
