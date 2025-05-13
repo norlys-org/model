eead19f3ef05155c87d55d1946e2e4856b50b410
Hugo Lageneste <hugo.lageneste@pm.me>
2025-05-13 20:02:54 +0200

    Plot two files

--- tests/plot.py
+++ tests/plot.py
@@ -5,29 +5,43 @@ import cartopy.crs as ccrs
 import json
 
 with open('tests/secs_py.json', 'r') as file:
-    data = json.load(file)
+    data1 = json.load(file)
+lons1 = np.array([v['lon'] for v in data1])
+lats1 = np.array([v['lat'] for v in data1])
+values1 = np.array([v['i'] for v in data1])
 
-lons = np.array([ v['lon'] for v in data ])
-lats = np.array([ v['lat'] for v in data ])
-values = np.array([ v['i'] for v in data ])
+with open('tests/secs_rs.json', 'r') as file:
+    data2 = json.load(file)
+lons2 = np.array([v['lon'] for v in data2])
+lats2 = np.array([v['lat'] for v in data2])
+values2 = np.array([v['i'] for v in data2])
 
-plt.figure(figsize=(10, 10))
-ax = plt.axes(projection=ccrs.Orthographic(central_longitude=0, central_latitude=90))
+fig = plt.figure(figsize=(20, 10))
 
-ax.coastlines()
-ax.gridlines()
-
+ax1 = fig.add_subplot(1, 2, 1, projection=ccrs.Orthographic(central_longitude=0, central_latitude=90))
+ax1.coastlines()
+ax1.gridlines()
 cmap = cm.Reds
+scatter1 = ax1.scatter(lons1, lats1,
+                      transform=ccrs.PlateCarree(),
+                      c=values1,
+                      cmap=cmap,
+                      vmin=0, vmax=400,
+                      s=50)
+plt.colorbar(scatter1, ax=ax1, label='Value (0-500)')
+ax1.set_title('File 1: Orthographic Projection Over North Pole')
 
-scatter = ax.scatter(lons, lats, 
-                     transform=ccrs.PlateCarree(),
-                     c=values, 
-                     cmap=cmap,
-                     vmin=0, vmax=400,
-                     s=50)
+ax2 = fig.add_subplot(1, 2, 2, projection=ccrs.Orthographic(central_longitude=0, central_latitude=90))
+ax2.coastlines()
+ax2.gridlines()
+scatter2 = ax2.scatter(lons2, lats2,
+                      transform=ccrs.PlateCarree(),
+                      c=values2,
+                      cmap=cmap,
+                      vmin=0, vmax=400,
+                      s=50)
+plt.colorbar(scatter2, ax=ax2, label='Value (0-500)')
+ax2.set_title('File 2: Orthographic Projection Over North Pole')
 
-plt.colorbar(scatter, label='Value (0-500)')
-
-plt.title('Orthographic Projection Over North Pole')
 plt.tight_layout()
 plt.show()
