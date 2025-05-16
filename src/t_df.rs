5cead2365c74f99af3c61b63ada025c5ef39034e
Hugo Lageneste <hugo@testnor.com>
2025-05-16 11:00:46 +0200

    Implement t_df_under with ndarray

--- src/t_df.rs
+++ src/t_df.rs
@@ -1,6 +1,26 @@
-use crate::{grid::GeographicalPoint, secs::R_EARTH, sphere::calc_angular_distance_and_bearing};
+use crate::{grid::GeographicalPoint, sphere::calc_angular_distance_and_bearing};
+use ndarray::{Array1, Array2, ArrayView1};
 
 const MU0: f64 = 1e-7;
+pub const R_EARTH: f64 = 6371e3;
+
+fn calc_t_df_under(
+    obs_r: &Array1<f64>,
+    sec_r: &Array1<f64>,
+    cos_theta: &Array1<f64>,
+) -> (Array1<f64>, Array1<f64>) {
+    let x = sec_r / obs_r;
+    let factor =
+        1.0 / (1.0 - 2.0 * x.clone() * cos_theta + x.mapv(|val| val.powi(2))).mapv(f64::sqrt);
+
+    // Amm & Viljanen: Equation 9
+    let br = MU0 / obs_r * (factor.clone() - 1.0);
+
+    // Amm & Viljanen: Equation 10
+    let btheta = -MU0 / obs_r * (factor * (x - cos_theta) + cos_theta);
+
+    (br, btheta)
+}
 
 pub fn t_df(obs_locs: &[GeographicalPoint], secs_locs: &[GeographicalPoint]) {
     let nobs = obs_locs.len();
@@ -15,18 +35,46 @@ pub fn t_df(obs_locs: &[GeographicalPoint], secs_locs: &[GeographicalPoint]) {
 
     let (theta, alpha) = calc_angular_distance_and_bearing(&obs_lat_lon, &secs_lat_lon);
 
-    let sin_theta = theta
-        .iter()
-        .map(|row| row.iter().map(|&x| x.sin()).collect())
-        .collect();
-    let cos_theta = theta
-        .iter()
-        .map(|row| row.iter().map(|&x| x.cos()).collect())
-        .collect();
+    let theta_array = Array2::from_shape_fn((nobs, nsec), |(i, j)| theta[i][j]);
+    let sin_theta = theta_array.mapv(|x| x.sin());
+    // Flatten + cos
+    let cos_theta = Array1::from_iter(theta.iter().flat_map(|row| row.iter().map(|&x| x.cos())));
 
-    let x: Vec<f64> = obs_locs
-        .iter()
-        .zip(secs_locs.iter())
-        .map(|(obs, sec)| obs.altitude / sec.altitude)
-        .collect(); // only implement lower
+    let obs_r = Array1::from_iter(obs_locs.iter().map(|obs| R_EARTH + obs.altitude));
+    let sec_r = Array1::from_iter(secs_locs.iter().map(|sec| R_EARTH + sec.altitude));
+
+    let (br, btheta) = calc_t_df_under(&obs_r, &sec_r, &cos_theta);
+
+    println!("{:?}", sin_theta);
+    println!("{:?}", cos_theta);
+    println!("Br: {:?}", br);
+    println!("Btheta: {:?}", btheta);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_t_df() {
+        t_df(
+            &[
+                GeographicalPoint {
+                    latitude: 10.0,
+                    longitude: 20.0,
+                    altitude: 3000.0,
+                },
+                GeographicalPoint {
+                    latitude: 11.0,
+                    longitude: 21.0,
+                    altitude: 3000.0,
+                },
+            ],
+            &[GeographicalPoint {
+                latitude: 40.0,
+                longitude: 60.0,
+                altitude: 4000.0,
+            }],
+        );
+    }
 }
