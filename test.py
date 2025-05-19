Fix t_df computation and add test

test.py
@@ -151,9 +151,6 @@ def T_df(obs_loc: np.ndarray, sec_loc: np.ndarray) -> np.ndarray:
         # over_locs is a 2d array of booleans
         under_indices = np.where(under_locs)
         obs_r = obs_loc[under_indices[0], 2]
-        print(obs_loc)
-        print(under_indices[0])
-        print(obs_r)
         sec_r = sec_loc[under_indices[1], 2]
         Br[under_locs], Btheta[under_locs] = _calc_T_df_under(
             obs_r, sec_r, cos_theta[under_locs]
@@ -175,7 +172,15 @@ def T_df(obs_loc: np.ndarray, sec_loc: np.ndarray) -> np.ndarray:
     return T
 
 
-T_df(
-    np.array([np.array([50, 20, 3000]), np.array([51, 21, 3000])]),
-    np.array([np.array([10, 30, 4000]), np.array([11, 31, 4000]), np.array([12, 32, 4000])]),
+print(
+    T_df(
+        np.array([np.array([50, 20, 3000]), np.array([51, 21, 3000])]),
+        np.array(
+            [
+                np.array([10, 30, 4000]),
+                np.array([11, 31, 4000]),
+                np.array([12, 32, 4000]),
+            ]
+        ),
+    )
 )