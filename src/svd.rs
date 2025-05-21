6169381b257e395e57ae2d1b0bcd31c72b3b82fe
Hugo Lageneste <hugo@testnor.com>
2025-05-21 13:46:08 +0200

    Reshape Transfer matrix ahead of storing it in class

--- src/svd.rs
+++ src/svd.rs
@@ -1,3 +1,6 @@
 use ndarray::Array3;
+use ndarray_linalg::SVD;
 
-fn svd(t_obs_flat: Array3<f64>, epsilon: f64) {}
+fn svd(t_obs_flat: &Array3<f64>, epsilon: f64) {
+    // t_obs_flat.svd(true, true);
+}
