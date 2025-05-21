234011371f6418ae2eb79237c522c005329f5ffe
Hugo Lageneste <hugo.lageneste@pm.me>
2025-05-21 19:03:11 +0200

    Truncate the svd results depending on epsilon validity

--- src/svd.rs
+++ src/svd.rs
@@ -1,5 +1,5 @@
 use nalgebra::{DMatrix, SVD};
-use ndarray::{Array2, Array3};
+use ndarray::{s, Array2, Array3};
 
 pub fn svd(t_obs_flat: &Array2<f64>, epsilon: f64) {
     // Convert ndarray Array2 to nalgebra DMatrix
@@ -10,14 +10,29 @@ pub fn svd(t_obs_flat: &Array2<f64>, epsilon: f64) {
     let svd = SVD::new(matrix, true, true);
     let (u, s, vh) = (svd.u.unwrap(), svd.singular_values, svd.v_t.unwrap());
 
-    let valid = s
+    let valid_indices: Vec<usize> = s
         .iter()
-        .map(|&val| val >= epsilon * s.max())
-        .collect::<Vec<bool>>();
+        .enumerate()
+        .filter_map(|(i, &val)| {
+            if val >= epsilon * s.max() {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .collect();
+    let u = Array2::from_shape_fn((u.nrows(), u.ncols()), |(i, j)| u[(i, j)]);
+    let vh = Array2::from_shape_fn((vh.nrows(), vh.ncols()), |(i, j)| vh[(i, j)]);
 
-    println!("{:?}", valid);
+    let u_valid = valid_indices
+        .iter()
+        .map(|&idx| u.slice(s![.., idx]).to_owned())
+        .collect::<Vec<_>>();
 
-    // let s_max = s.iter().fold(0.0, |max, &val| f64::max(max, val));
-    // let valid = s.mapv(|val| val >= epsilon * s_max);
-    // println!("{:?}", valid);
+    let vh_valid = valid_indices
+        .iter()
+        .map(|&idx| vh.slice(s![idx, ..]).to_owned())
+        .collect::<Vec<_>>();
+
+    let w_values: Vec<f64> = valid_indices.iter().map(|&idx| 1.0 / s[idx]).collect();
 }
