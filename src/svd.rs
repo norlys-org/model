16d9cd76d3edcfd4a139726aa7ff61cf221c9fd8
Hugo Lageneste <hugo@testnor.com>
2025-05-21 15:00:01 +0200

    Start implementing svd

--- src/svd.rs
+++ src/svd.rs
@@ -1,6 +1,6 @@
-use ndarray::Array3;
+use ndarray::{Array2, Array3};
 use ndarray_linalg::SVD;
 
-fn svd(t_obs_flat: &Array3<f64>, epsilon: f64) {
-    // t_obs_flat.svd(true, true);
+fn svd(t_obs_flat: &Array2<f64>, epsilon: f64) {
+    let (u, s, vh) = t_obs_flat.svd(true, true).unwrap();
 }
