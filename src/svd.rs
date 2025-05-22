4a7d078a63c1eee595009e3ea9c62201dae79776
Hugo Lageneste <hugo@testnor.com>
2025-05-22 14:28:40 +0200

    Finish SVD implementation

--- src/svd.rs
+++ src/svd.rs
@@ -1,5 +1,5 @@
 use nalgebra::{DMatrix, SVD};
-use ndarray::{s, Array2, Array3};
+use ndarray::{Array1, Array2};
 
 pub fn svd(t_obs_flat: &Array2<f64>, epsilon: f64) -> Array2<f64> {
     // Convert ndarray Array2 to nalgebra DMatrix
@@ -21,28 +21,23 @@ pub fn svd(t_obs_flat: &Array2<f64>, epsilon: f64) -> Array2<f64> {
             }
         })
         .collect();
-    let u = Array2::from_shape_fn((u.nrows(), u.ncols()), |(i, j)| u[(i, j)]);
-    let vh = Array2::from_shape_fn((vh.nrows(), vh.ncols()), |(i, j)| vh[(i, j)]);
 
-    let u = valid_indices
-        .iter()
-        .map(|&idx| u.slice(s![.., idx]).to_owned())
-        .collect::<Vec<_>>();
+    let u_t = Array2::from_shape_fn((u.nrows(), valid_indices.len()), |(i, j)| {
+        u[(i, valid_indices[j])]
+    })
+    .t()
+    .to_owned();
 
-    let vh = valid_indices
-        .iter()
-        .map(|&idx| vh.slice(s![idx, ..]).to_owned())
-        .collect::<Vec<_>>();
+    let vh_t = Array2::from_shape_fn((valid_indices.len(), vh.ncols()), |(i, j)| {
+        vh[(valid_indices[i], j)]
+    })
+    .t()
+    .to_owned();
 
-    // let w_values: Vec<f64> = valid_indices.iter().map(|&idx| 1.0 / s[idx]).collect();
-    //
-    // let w_diag = Array2::from_diag(&ndarray::Array1::from(w_values));
-    //
-    // // Vh.T @ (W @ U.T)
-    // let u_t = u.t().to_owned();
-    // let w_u_t = w_diag.dot(&u_t);
-    // let vh_t = vh.t().to_owned();
-    //
-    // // Return the final result
-    // vh_t.dot(&w_u_t)
+    let w_diag = Array2::from_diag(&Array1::from_iter(
+        valid_indices.iter().map(|&idx| 1.0 / s[idx]),
+    ));
+
+    // Vh.T @ (W @ U.T)
+    vh_t.dot(&w_diag.dot(&u_t))
 }
