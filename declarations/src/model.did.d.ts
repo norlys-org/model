f41571400da0d47e62576a1384bcf0a6a9edbe1e
Hugo Lageneste <hugo.lageneste@pm.me>
2025-06-10 16:26:10 +0200

    Create declarations package

---
+++ declarations/src/model.did.d.ts
@@ -0,0 +1,22 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
+
+export interface ObservationVector {
+  'i' : number,
+  'j' : number,
+  'k' : number,
+  'lat' : number,
+  'lon' : number,
+}
+export interface _SERVICE {
+  'a_add_authorized_user' : ActorMethod<[Principal], undefined>,
+  'a_initialize_authorized_user' : ActorMethod<[Principal], undefined>,
+  'a_list_authorized_users' : ActorMethod<[], Array<Principal>>,
+  'a_remove_authorized_user' : ActorMethod<[Principal], undefined>,
+  'm_fit_obs' : ActorMethod<[Array<ObservationVector>], undefined>,
+  'm_fit_pred' : ActorMethod<[], undefined>,
+  'm_predict' : ActorMethod<[], Uint8Array | number[]>,
+}
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
---
+++ declarations/src/model.did.js
@@ -0,0 +1,23 @@
+export const idlFactory = ({ IDL }) => {
+  const ObservationVector = IDL.Record({
+    'i' : IDL.Float64,
+    'j' : IDL.Float64,
+    'k' : IDL.Float64,
+    'lat' : IDL.Float64,
+    'lon' : IDL.Float64,
+  });
+  return IDL.Service({
+    'a_add_authorized_user' : IDL.Func([IDL.Principal], [], []),
+    'a_initialize_authorized_user' : IDL.Func([IDL.Principal], [], []),
+    'a_list_authorized_users' : IDL.Func(
+        [],
+        [IDL.Vec(IDL.Principal)],
+        ['query'],
+      ),
+    'a_remove_authorized_user' : IDL.Func([IDL.Principal], [], []),
+    'm_fit_obs' : IDL.Func([IDL.Vec(ObservationVector)], [], []),
+    'm_fit_pred' : IDL.Func([], [], []),
+    'm_predict' : IDL.Func([], [IDL.Vec(IDL.Nat8)], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
